eb09e0f53a616745fdd8851746e5a71ebc72f340
Emrys Shevek <mpoggemann96@gmail.com>
2022-10-04 12:58:57 -0700

    learning rate decay

--- tictactoe/train.py
+++ tictactoe/train.py
@@ -20,6 +20,7 @@ rewards = {
 }
 epsilon_decay = 1 - 1e-6
 discount_decay = 1 - 1e-6
+learning_decay = 1 - 1e-6
 
 def train():
     global epsilon_decay
@@ -28,7 +29,7 @@ def train():
     # starting variables
     vars = {
         'epsilon': 1,
-        'learning_rate': .1,
+        'learning_rate': 0.01,
         'discount_factor': 0,
     }
 
@@ -44,7 +45,7 @@ def train():
     for i in range(1, max_games+1):
         if i == 1 or i % checkpoint_frequency == 0:
             print(f"*** GAME {i} ***")
-            print(f"state size: {len(q_table)}, epsilon={vars['epsilon']:.2f}, discount={vars['discount_factor']}")
+            print(f"state size: {len(q_table)}, epsilon={vars['epsilon']:.2f}, discount={vars['discount_factor']}, learning rate={vars['learning_rate']}")
             print(f"cp time={(time.time() - checkpoint_time):.2f} sec, total time={time.time() - start_time:.2f} sec")
             for k, v in random.sample(list(q_table.items()), k=min(10, len(q_table))):
                 print(f"  {k}:\t{v}")
@@ -60,6 +61,7 @@ def train():
 
         vars['epsilon'] *= epsilon_decay
         vars['discount_factor'] = 1 - ((1 - vars['discount_factor']) * discount_decay)
+        vars['learning_rate'] = 1 - ((1 - vars['learning_rate']) * learning_decay)
 
     print("Final result:")
     for k, v in random.sample(list(q_table.items()), k=50):
