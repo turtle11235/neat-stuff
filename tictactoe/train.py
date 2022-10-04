2a30ed7eeb108b1c6c86b2cfbb9b6186e4fabdc1
Emrys Shevek <mpoggemann96@gmail.com>
2022-10-04 12:25:59 -0700

    added discount stepping

--- tictactoe/train.py
+++ tictactoe/train.py
@@ -8,7 +8,7 @@ import json
 
 
 # constants
-initial_q = 1
+initial_q = 0
 max_games = 5000000
 checkpoint_frequency = 50000
 rewards = {
@@ -18,18 +18,18 @@ rewards = {
     'bad_move': -20,
     'move': -1
 }
-epsilon_decay = 1 - 1e-6
-
-
+epsilon_decay = 1 - 1e-5
+discount_step = 1e-10
 
 def train():
     global epsilon_decay
+    global discount_step
 
     # starting variables
     vars = {
         'epsilon': 1,
-        'learning_rate': .3,
-        'discount_factor': .3,
+        'learning_rate': .1,
+        'discount_factor': 0,
     }
 
     q_table = defaultdict(lambda: [initial_q] * 9)
@@ -43,7 +43,9 @@ def train():
 
     for i in range(1, max_games+1):
         if i == 1 or i % checkpoint_frequency == 0:
-            print(f"*** GAME {i} ***\nstate size: {len(q_table)}, epsilon={vars['epsilon']:.2f}, cp time={(time.time() - checkpoint_time):.2f} sec, total time={time.time() - start_time:.2f} sec")
+            print(f"*** GAME {i} ***")
+            print(f"state size: {len(q_table)}, epsilon={vars['epsilon']:.2f}, discount={vars['discount_factor']}")
+            print(f"cp time={(time.time() - checkpoint_time):.2f} sec, total time={time.time() - start_time:.2f} sec")
             for k, v in random.sample(list(q_table.items()), k=min(10, len(q_table))):
                 print(f"  {k}:\t{v}")
             print()
@@ -57,6 +59,7 @@ def train():
         TicTacToe().play(player1, player2)
 
         vars['epsilon'] *= epsilon_decay
+        vars['discount_factor'] = min(1, vars['discount_factor'] + discount_step)
 
     print("Final result:")
     for k, v in random.sample(list(q_table.items()), k=50):
